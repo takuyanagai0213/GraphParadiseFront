bc408cd7dba34e3cf2115a171abe3986e7f343fd
nagai takuya <nagai.takuya@cimx-initiative.com>
2021-08-27 16:45:55 +0900

    ヘッダー追加

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <Header/>
   <div class="container">
     <form action="/login" method="post">
       ユーザ名:<input type="text" name="username">
@@ -8,3 +8,24 @@
      </form>
   </div>
 </template>
+<script>
+import Header from './../../components/Header'
+// import axios from 'axios'
+
+export default {
+  name: 'app',
+  components : {
+    Header
+  },
+  data() {
+    return {
+      message: null
+    }
+  },
+  computed() {
+
+  },
+  methods: {
+  }
+}
+</script>
