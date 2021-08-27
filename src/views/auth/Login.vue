9027220031381fbfedbfa7f7c549bfcd78854c1c
nagai takuya <nagai.takuya@cimx-initiative.com>
2021-08-27 16:50:54 +0900

    ログインページのスタイルを調整

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,39 @@
 <template>
   <Header/>
-  <div class="container">
-    <form action="/login" method="post">
-      ユーザ名:<input type="text" name="username">
-      パスワード:<input type="password" name="password">
-      <input type="submit" value="ログイン">
-     </form>
+
+<section class="hero is-primary is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+          <form action="/login" method="post" class="box">
+            <div class="field">
+              <label for="" class="label">ユーザ名</label>
+              <div class="control has-icons-left">
+                <input type="text" name="username" class="input" required>
+                <span class="icon is-small is-left">
+                  <i class="fa fa-envelope"></i>
+                </span>
+              </div>
+            </div>
+            <div class="field">
+              <label for="" class="label">パスワード</label>
+              <div class="control has-icons-left">
+                <input type="password" placeholder="*******" class="input" required>
+                <span class="icon is-small is-left">
+                  <i class="fa fa-lock"></i>
+                </span>
+              </div>
+            </div>
+            <div class="field">
+                <input type="submit" value="ログイン" class="button is-success">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
+</section>
 </template>
 <script>
 import Header from './../../components/Header'
