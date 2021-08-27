a9391771b1cfb50e875a41bdd3e4f6d77bd2e1f3
nagai takuya <nagai.takuya@cimx-initiative.com>
2021-08-27 16:34:54 +0900

    不要なフォームを削除

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -16,34 +16,6 @@
     </div>
   </div>
   <div class="field">
-    <label class="label">ユーザID</label>
-    <div class="control has-icons-left has-icons-right">
-      <input class="input is-success" type="text"  name="user_name" placeholder="Text input" value="bulma">
-      <span class="icon is-small is-left">
-        <i class="fas fa-user"></i>
-      </span>
-      <span class="icon is-small is-right">
-        <i class="fas fa-check"></i>
-      </span>
-    </div>
-    <p class="help is-success">This username is available</p>
-  </div>
-  
-  <div class="field">
-    <label class="label">メールアドレス</label>
-    <div class="control has-icons-left has-icons-right">
-      <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-      <span class="icon is-small is-left">
-        <i class="fas fa-envelope"></i>
-      </span>
-      <span class="icon is-small is-right">
-        <i class="fas fa-exclamation-triangle"></i>
-      </span>
-    </div>
-    <p class="help is-danger">This email is invalid</p>
-  </div>
-  
-  <div class="field">
     <label class="label">パスワード</label>
     <div class="control has-icons-left has-icons-right">
       <input class="input is-danger" type="password"  name="password" v-model="password" placeholder="パスワードを入力してください">
@@ -56,48 +28,6 @@
     </div>
     <p class="help is-danger">This パスワード is invalid</p>
   </div>
-  
-  <div class="field">
-    <label class="label">Subject</label>
-    <div class="control">
-      <div class="select">
-        <select>
-          <option>Select dropdown</option>
-          <option>With options</option>
-        </select>
-      </div>
-    </div>
-  </div>
-  
-  <div class="field">
-    <label class="label">Message</label>
-    <div class="control">
-      <textarea class="textarea" placeholder="Textarea"></textarea>
-    </div>
-  </div>
-  
-  <div class="field">
-    <div class="control">
-      <label class="checkbox">
-        <input type="checkbox">
-        I agree to the <a href="#">terms and conditions</a>
-      </label>
-    </div>
-  </div>
-  
-  <div class="field">
-    <div class="control">
-      <label class="radio">
-        <input type="radio" name="question">
-        Yes
-      </label>
-      <label class="radio">
-        <input type="radio" name="question">
-        No
-      </label>
-    </div>
-  </div>
-  
   <div class="field is-grouped">
     <div class="control">
       <button class="button is-link" v-on:click="createUser">Submit</button>
@@ -106,7 +36,6 @@
       <button class="button is-link is-light">Cancel</button>
     </div>
   </div>
-  {{info}}
   </section>
 </template>
 <script>
