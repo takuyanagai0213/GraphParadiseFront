4cbfd3b6aab6744df5c0663377ef142cf0163a5f
nagai takuya <nagai.takuya@cimx-initiative.com>
2021-08-27 15:56:12 +0900

    新規vueファイルを追加

---
+++ src/views/UserList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+    <div class="content" id="user_list">
+    </div>
+    <div class="content">
+      <div class="field is-grouped">
+        <div class="control">
+            <button class="button is-link" id="submit">Submit</button>
+          </div>
+          <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
